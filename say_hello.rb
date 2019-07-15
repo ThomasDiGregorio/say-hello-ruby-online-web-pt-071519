aee7ba9bdfd0d62dbc366ac259fbb734aa4ad9e0
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-07-15 18:47:00 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,9 @@
 # Build your say_hello method here
 # I call on the method, say_hello, and give it the string "Richard"
-say_hello("Richard")
+
 
 def say_hello(name)
-  puts name
-end 
+  puts name 
+end  
+  say_hello("Richard")
+ 
